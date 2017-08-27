dfe4b0eb0c6faaafc3c4ee822724900cabcf30f5
hiEntropy <austin.r.tipton@gmail.com>
2017-08-27 15:24:03 -0700

    things connect now and thats good... can serve content. Need to make searches not case sensitive

--- Query.py
+++ Query.py
@@ -1,28 +1,38 @@
-from bson.json_util import dumps
+from bson.json_util import dumps, loads
 import pymongo
 
-def get_connection_to_collection(self, user_id, password, database, collection):
-    mongodb_url = "mongodb://" + user_id + ":" + password + "@ds019708.mlab.com:19708/" + database
+
+def get_connection_to_collection(user_id, password, database, collection):
+    #mongodb://<dbuser>:<dbpassword>@ds019708.mlab.com:19708/recipes
+    mongodb_url = "".join(["mongodb://", user_id, ":", password, "@ds019708.mlab.com:19708/", database])
     client = pymongo.MongoClient(mongodb_url)
     db = client[database]
     return db[collection]
 
-def get_by_nmae(title):
+
+def get_by_name(title):
     try:
-        collection = get_connection_to_collection(*get_mongo_creds())
+        creds = get_mongo_creds()
+        collection = get_connection_to_collection(*creds)
         response = collection.find({"title": title})
         return dumps(response)
     except:
         return dumps({"ERROR": "QUERY FAILED"})
 
-def get_by_ingredients(self, param):
+
+
+def get_by_ingredients(ingredients):
     try:
         collection = get_connection_to_collection(*get_mongo_creds())
         response = ''
-        return dumps(response)
+        response = dumps(response)
     except:
         return dumps({"ERROR": "QUERY FAILED"})
 
+
 def get_mongo_creds():
-    #This needs to reach out to S3 for credentials that will be stored there
-    return ["hiEntropy","S~lm*n376~Atomic360!","recipes","drinks"]
+    creds = ""
+    with open("./creds.json") as file:
+        creds = file.read()
+    creds = loads(creds)
+    return [creds.get("username"), creds.get("password"), creds.get("db"), creds.get("collection")]
--- drinkApi.py
+++ drinkApi.py
@@ -1,35 +1,40 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, abort
-from Query import get_by_name,get_by_ingredient
+from flask_restful import Resource, Api, abort, reqparse
+from Query import get_by_name, get_by_ingredients
 
 app = Flask(__name__)
 api = Api(app)
 
 
 class DrinksByName(Resource):
-    def get(self,name):
-        if not valid_key(request.form.get("key")):
+    parser = reqparse.RequestParser()
+    parser.add_argument("key", type=str, location="headers", required=True, help="Send a valid key to access this "
+                                                                                 "endpoint")
+
+    def get(self, name):
+        args = self.parser.parse_args()
+        if not valid_key(args.get("key")):
             abort_404()
         return get_by_name(name)
 
+
 class DrinksByIngredient(Resource):
-    def get(self,ingredient):
+    def get(self, ingredient):
         if not valid_key(request.form.get("key")):
             abort_404()
-        return get_by_ingredient(ingredient)
+        return get_by_ingredients(ingredient)
 
 
 def valid_key(key):
     return True
 
+
 def abort_404():
     abort(404, message="Authentication is required")
 
-api.add_resource(DrinksByName,"/<string:name>")
-api.add_resource(DrinksByIngredient,"/<string:ingredient>")
 
-
-
+api.add_resource(DrinksByName, "/name/<string:name>")
+api.add_resource(DrinksByIngredient, "/ingredient/<string:ingredient>")
 
 if __name__ == '__main__':
     app.run(debug=True)
